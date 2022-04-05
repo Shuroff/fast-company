f1d5f94e07b89976507c80374240463f21e7264c
Alexander Levterov <shuroffrashiday@yandex.ru>
2022-04-05 17:52:52 +0300

    pagination complete

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -13,24 +13,26 @@ const Users = () => {
   return (
     <>
       <SearchStatus numOfUsers={users.length} />
-      <table className='table'>
-        <thead>
-          <tr>
-            <th scope='col'>Имя</th>
-            <th scope='col'>Качества</th>
-            <th scope='col'>Профессия</th>
-            <th scope='col'>Встретился, раз</th>
-            <th scope='col'>Оценка</th>
-            <th scope='col'>Избранное</th>
-            <th scope='col'></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => {
-            return <User key={user._id} user={user} onDelete={handleDelete} />
-          })}
-        </tbody>
-      </table>
+      {users.length > 0 && (
+        <table className='table'>
+          <thead>
+            <tr>
+              <th scope='col'>Имя</th>
+              <th scope='col'>Качества</th>
+              <th scope='col'>Профессия</th>
+              <th scope='col'>Встретился, раз</th>
+              <th scope='col'>Оценка</th>
+              <th scope='col'>Избранное</th>
+              <th scope='col'></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => {
+              return <User key={user._id} user={user} onDelete={handleDelete} />
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
