09ab905c852eca3604a50dae2748324fb10c6d6e
Your Name <you@example.com>
2022-05-11 16:45:10 +0300

    9.1.6

--- src/layouts/login/login.jsx
+++ src/layouts/login/login.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from 'react'
+import TextField from '../../components/textField/textField'
 const Login = () => {
-  return Login
+  const [data, setData] = useState({ email: '', password: '' })
+  const [errors, setErrors] = useState()
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const isValid = validate()
+    if (!isValid) return
+    console.log(data)
+  }
+
+  useEffect(() => {
+    validate()
+  }, [data])
+
+  const validate = () => {
+    const errors = {}
+    for (const fieldName in data) {
+      if (data[fieldName].trim() === '') {
+        errors[fieldName] = `${fieldName} обязательно дял заполнения`
+      }
+    }
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={data.email}
+        name='email'
+        onChange={handleChange}
+        label='Электронная почта'
+      />
+      <TextField
+        value={data.password}
+        type='password'
+        name='password'
+        onChange={handleChange}
+        label='Пароль'
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  )
 }
 
 export default Login
