0d8695d5c0d6de9a44bc75de9252696c33902330
AlexanderLevterov <alevterov@inbox.ru>
2022-06-26 20:16:19 +0300

    15 complete

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,10 +1,39 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
-import config from '../utils/config.json'
+import configFile from '../utils/config.json'
 
-axios.defaults.baseURL = config.apiEndpoint
+axios.defaults.baseURL = configFile.apiEndpoint
+axios.interceptors.request.use(
+  function (config) {
+    console.log(configFile)
+    if (configFile.isFirebase) {
+      const containSlash = /\/$/gi.test(config.url)
+      config.url = containSlash
+        ? config.url.slice(0, -1) + '.json'
+        : config.url + '.json'
+    }
+
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+function transformData(data) {
+  return data
+    ? Object.keys(data).map(key => ({
+        ...data[key],
+      }))
+    : []
+}
 axios.interceptors.response.use(
-  res => res,
+  res => {
+    if (configFile.isFirebase) {
+      res.data = { content: transformData(res.data) }
+    }
+
+    return res
+  },
   function (error) {
     const expectedErrors =
       error.response &&
@@ -17,6 +46,7 @@ axios.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+
 const httpService = {
   get: axios.get,
   post: axios.post,
