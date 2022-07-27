244c8b1df898c0a87911d1f2dde22f8e5b30658b
Your Name <you@example.com>
2022-07-27 12:39:11 +0300

    '5.7_try_to_mongoDB'

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const PORT = config.get('port') ?? 8080
+
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Production')
+// } else {
+//   console.log('Development')
+// }
+
+// user: lev
+// password: fast-cmp-lev
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'))
+    console.log('MongoDB connected')
+    app.listen(PORT, () => {
+      console.log(
+        chalk.green(`Server has been started on port ${PORT}
+			`)
+      )
+    })
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+  }
+}
+start()
