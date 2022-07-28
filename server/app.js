dc2cd362ae6e8ff740d264aeade70788afad3f51
Alexander <shuroffrashiday@yandex.ru>
2022-07-28 13:24:32 +0300

    'mongoose_can't_connect_behind_proxy'

--- server/app.js
+++ server/app.js
@@ -1,17 +1,12 @@
-var globalTunnel = require('global-tunnel-ng')
-
-globalTunnel.initialize({
-  host: '10.33.101.131',
-  port: 58228,
-  proxyAuth: 'shde33:shd121', // optional authentication
-})
-process.env.http_proxy = 'http://shde33:shd121@10.33.101.131:58228'
-process.env.https_proxy = 'http://shde33:shd121@10.33.101.131:58228'
-globalTunnel.initialize()
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
+const request = require('request')
+
+mongoose
+process.env.HTTP_PROXY = 'http://127.0.0.1:20307'
+process.env.HTTPS_PROXY = 'http://127.0.0.1:20307'
 
 const app = express()
 
@@ -20,14 +15,17 @@ app.use(express.urlencoded({ extended: false }))
 
 const PORT = config.get('port') ?? 8080
 
-// if (process.env.NODE_ENV === 'production') {
-//   console.log('Production')
-// } else {
-//   console.log('Development')
-// }
-
-// user: lev
-// password: fast-cmp-lev
+request(
+  'http://n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017',
+  function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body)
+    }
+    if (error) {
+      console.log(error)
+    }
+  }
+)
 async function start() {
   try {
     await mongoose.connect(
